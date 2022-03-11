0612b7670861691dbe573b97fa37c67022f99e99
DariaSe <daria.samosledova@icloud.com>
2022-03-11 15:04:11 +0300

    Initial Commit

---
+++ ToDoGameClean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoGameClean
+//
+//  Created by Дарья Селезнёва on 11.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
